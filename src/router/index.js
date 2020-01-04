313e19b54ce062c88fc20fc87f570d720ccea97d
zzhangting <zt18434763734@163.com>
2020-01-04 10:50:34 +0800

    master commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+//我是组长
 import Vue from 'vue'
 import Router from 'vue-router'
 
@@ -56,110 +57,77 @@ export const constantRoutes = [
   },
 
   {
-    path: '/example',
+    path: '/customer',
     component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
+    children: [{
+      path: 'List',
+      name: 'CustomerList',
+      component: () => import('@/pages/customer/List'),
+      meta: { title: '顾客管理', icon: 'dashboard' }
+    }]
   },
-
   {
-    path: '/form',
+    path: '/employee',
     component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
+    children: [{
+      path: 'List',
+      name: 'EmployeeList',
+      component: () => import('@/pages/employee/List'),
+      meta: { title: '员工管理', icon: 'tree' }
+    }]
   },
-
   {
-    path: '/nested',
+    path: '/product',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
-      }
-    ]
+    children: [{
+      path: 'List',
+      name: 'ProductList',
+      component: () => import('@/pages/product/List'),
+      meta: { title: '产品管理', icon: 'example' }
+    }]
   },
-
   {
-    path: 'external-link',
+    path: '/order',
     component: Layout,
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
+    children: [{
+      path: 'List',
+      name: 'OrderList',
+      component: () => import('@/pages/order/List'),
+      meta: { title: '订单管理', icon: 'table' }
+    }]
+  },
+  {
+    path: '/address',
+    component: Layout,
+    children: [{
+      path: 'List',
+      name: 'AddressList',
+      component: () => import('@/pages/address/List'),
+      meta: { title: '地址管理', icon: 'tree' }
+    }]
+  },
+  {
+    path: '/category',
+    component: Layout,
+    children: [{
+      path: 'List',
+      name: 'CategoryrList',
+      component: () => import('@/pages/category/List'),
+      meta: { title: '栏目管理', icon: 'tree' }
+    }]
+  },
+  {
+    path: '/comment',
+    component: Layout,
+    children: [{
+      path: 'List',
+      name: 'CommentList',
+      component: () => import('@/pages/comment/List'),
+      meta: { title: '评论管理', icon: 'tree' }
+    }]
   },
 
-  // 404 page must be placed at the end !!!
+  
   { path: '*', redirect: '/404', hidden: true }
 ]
 
@@ -178,3 +146,141 @@ export function resetRouter() {
 }
 
 export default router
+
+// import Vue from 'vue'
+// import Router from 'vue-router'
+
+// Vue.use(Router)
+
+// /* Layout */
+// import Layout from '@/layout'
+
+// /**
+//  * Note: sub-menu only appear when route children.length >= 1
+//  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+//  *
+//  * hidden: true                   if set true, item will not show in the sidebar(default is false)
+//  * alwaysShow: true               if set true, will always show the root menu
+//  *                                if not set alwaysShow, when item has more than one children route,
+//  *                                it will becomes nested mode, otherwise not show the root menu
+//  * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+//  * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+//  * meta : {
+//     roles: ['admin','editor']    control the page roles (you can set multiple roles)
+//     title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+//     icon: 'svg-name'             the icon show in the sidebar
+//     breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+//     activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+//   }
+//  */
+
+// /**
+//  * constantRoutes
+//  * a base page that does not have permission requirements
+//  * all roles can be accessed
+//  */
+// export const constantRoutes = [
+//   {
+//     path: '/login',
+//     component: () => import('@/views/login/index'),
+//     hidden: true
+//   },
+
+//   {
+//     path: '/404',
+//     component: () => import('@/views/404'),
+//     hidden: true
+//   },
+
+//   {
+//     path: '/',
+//     component: Layout,
+//     redirect: '/dashboard',
+//     children: [{
+//       path: 'dashboard',
+//       name: 'Dashboard',
+//       component: () => import('@/views/dashboard/index'),
+//       meta: { title: 'Dashboard', icon: 'dashboard' }
+//     }]
+//   },
+
+
+//   {
+//     path: '/customer',
+//     component: Layout,
+//     children: [
+//       {
+//         path: 'List',
+//         name: 'CustomerList',//name不可重复
+//         component: () => import('@/pages/customer/List'),//箭头函数的this，指向外部函数的this
+//         meta: { title: '顾客管理', icon: 'user' }
+//       }
+//     ]
+//   },
+//   {
+//     path: '/employee',
+//     component: Layout,
+//     children: [
+//       {
+//         path: 'List',
+//         name: 'EmployeeList',//name不可重复
+//         component: () => import('@/pages/employee/List'),//箭头函数的this，指向外部函数的this
+//         meta: { title: '员工管理', icon: 'tree' }
+//       }
+//     ]
+//   },
+//   {
+//     path: '/product',
+//     component: Layout,
+//     children: [
+//       {
+//         path: 'List',
+//         name: 'ProductList',//name不可重复
+//         component: () => import('@/pages/product/List'),//箭头函数的this，指向外部函数的this
+//         meta: { title: '产品管理', icon: 'table' }
+//       }
+//     ]
+//   },
+//   {
+//     path: '/category',
+//     component: Layout,
+//     children: [
+//       {
+//         path: 'List',
+//         name: 'CategoryList',//name不可重复
+//         component: () => import('@/pages/category/List'),//箭头函数的this，指向外部函数的this
+//         meta: { title: '栏目管理', icon: 'example' }
+//       }
+//     ]
+//   },
+//   {
+//     path: 'external-link',
+//     component: Layout,
+//     children: [
+//       {
+//         path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+//         meta: { title: 'External Link', icon: 'link' }
+//       }
+//     ]
+//   },
+
+//   // 404 page must be placed at the end !!!
+//   { path: '*', redirect: '/404', hidden: true }
+// ]
+
+// const createRouter = () => new Router({
+//   // mode: 'history', // require service support
+//   scrollBehavior: () => ({ y: 0 }),
+//   routes: constantRoutes
+// })
+
+// const router = createRouter()
+
+// // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+// export function resetRouter() {
+//   const newRouter = createRouter()
+//   router.matcher = newRouter.matcher // reset router
+// }
+
+// export default router
+
