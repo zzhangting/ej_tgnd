315a3ad99de11471b6345412162a43998fffc416
yyangxiaoru <yyangxiaoru@126.com>
2020-01-04 13:34:51 +0800

    杨晓茹修改了地址管理

--- src/pages/address/List.vue
+++ src/pages/address/List.vue
@@ -8,14 +8,15 @@
         <!--/按钮-->
         <!--表格-->
         <el-table :data="address">
-      <el-table-column   prop="province" label="省会"></el-table-column>
-      <el-table-column  prop="city" label="城市"></el-table-column>
-      <el-table-column prop="address" label="详细地址"></el-table-column>
-      <el-table-column prop="telephone" label="手机号"></el-table-column>
-            <el-table-column  label="操作">                                     
-              <template v-slot="slot"> 
-                 <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
-                 <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
+             <el-table-column prop="id" label="编号"></el-table-column>
+             <el-table-column   prop="province" label="省会"></el-table-column>
+             <el-table-column  prop="city" label="城市"></el-table-column>
+             <el-table-column prop="address" label="详细地址"></el-table-column>
+             <el-table-column prop="telephone" label="手机号"></el-table-column>
+             <el-table-column  label="操作">                                     
+                <template v-slot="slot"> 
+                    <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
+                    <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
               </template>
             </el-table-column>
         </el-table>
@@ -30,9 +31,6 @@
         width ="60%">
         测试:{{form}}
              <el-form  :model="form" label-width="80px">
-              <el-form-item label="编号">
-                  <el-input v-model="form.id"/>
-              </el-form-item>
               <el-form-item label="省份">
                   <el-input v-model="form.province"/> 
               </el-form-item>
@@ -45,12 +43,7 @@
               <el-form-item label="手机号">
                   <el-input v-model="form.telephone"/> 
               </el-form-item>
-              <el-form-item label="顾客编号">
-                  <el-input v-model="form.customer"/> 
-              </el-form-item>
             </el-form>
-
-
             <span slot="footer" class="dialog-footer">
                 <el-button size="small" @click="closeModalHandler">
                     取消
